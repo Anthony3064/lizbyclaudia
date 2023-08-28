75795371da4f523fd278d2c2b8a74773fcd99fad
Anthony3064 <anthonyestiven3064@gmail.com>
2023-08-27 21:04:24 -0600

    Se arregla error

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter >
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/lizbyclaudia' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import {  useState } from "react";
 import { Link } from "react-router-dom";
 import ButtonIcon from "./ButtonIcon";
 const menuLinks = {
@@ -21,18 +21,18 @@ const Navbar = () => {
 
     return (
         <nav className="nav">
-            <Link to='/' className="logo">
+            <Link to='/lizbyclaudia' className="logo">
                 <span>Liz</span><br /><span style={{ margin: '20px' }}>By</span><br /> <span style={{ margin: '30px' }}>Clau</span>
             </Link>
             <ul className={`nav__menu ${!showBars ? 'active' : ''}`}>
                 <li className={`nav__item ${linkActive === menuLinks.HOME && 'active'}`} >
-                    <Link className='nav__link' to='/' onClick={() => handleClickLinkActive(menuLinks.HOME)}>Inicio</Link>
+                    <Link className='nav__link' to='/lizbyclaudia' onClick={() => handleClickLinkActive(menuLinks.HOME)}>Inicio</Link>
                 </li>
                 <li className={`nav__item ${linkActive === menuLinks.SERVICES && 'active'}`} >
-                    <Link className='nav__link' to='/' onClick={() => handleClickLinkActive(menuLinks.SERVICES)}>Servicios</Link>
+                    <Link className='nav__link' to='/lizbyclaudia' onClick={() => handleClickLinkActive(menuLinks.SERVICES)}>Servicios</Link>
                 </li>
                 <li className={`nav__item ${linkActive === menuLinks.CONTACT && 'active'}`} >
-                    <Link className='nav__link' to='/' onClick={() => handleClickLinkActive(menuLinks.CONTACT)}>Contacto</Link>
+                    <Link className='nav__link' to='/lizbyclaudia' onClick={() => handleClickLinkActive(menuLinks.CONTACT)}>Contacto</Link>
                 </li>
                 <div className="social__network" style={{/*display: 'none'*/ }}>
                     <li className="nav__item">
