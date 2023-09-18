59b25337c32413a99c4e9ee8c57f526a9fd25298
Anthony3064 <anthonyestiven3064@gmail.com>
2023-09-17 20:31:49 -0600

    Se vuelve a cambiar las rutas de las imágenes

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,22 +1,22 @@
 export const FIRE_OUTLINE_MAKEUP = [
-    { src: '../lizbyclaudia/assets/images/fire-outline-makeup.jpg', alt: 'delineado-fuego.jpg' },
-    { src: '../lizbyclaudia/assets/images/fire-outline-makeup-close-eyes.jpg', alt: 'delineado-fuego-ojos-abiertos.jpg' },
+    { src: './lizbyclaudia/src/assets/images/fire-outline-makeup.jpg', alt: 'delineado-fuego.jpg' },
+    { src: './lizbyclaudia/src/assets/images/fire-outline-makeup-close-eyes.jpg', alt: 'delineado-fuego-ojos-abiertos.jpg' },
 ]
 
 export const BLUE_MAKEUP = [
-    { src: '../lizbyclaudia/assets/images/blue-makeup.jpg', alt: 'maquillaje-azul.jpg' },
-    { src: '../lizbyclaudia/assets/images/blue-makeup-complete.jpg', alt: 'maquillaje-azul-completo.jpg' },
+    { src: './lizbyclaudia/src/assets/images/blue-makeup.jpg', alt: 'maquillaje-azul.jpg' },
+    { src: './lizbyclaudia/src/assets/images/blue-makeup-complete.jpg', alt: 'maquillaje-azul-completo.jpg' },
 ]
 
 export const GREEN_MAKEUP = [
-    { src: '../lizbyclaudia/assets/images/green-makeup-1.jpg', alt: 'maquillaje-verde-1.jpg' },
-    { src: '../lizbyclaudia/assets/images/green-makeup-2.jpg', alt: 'maquillaje-verde-2.jpg' },
-    { src: '../lizbyclaudia/assets/images/green-makeup-3.jpg', alt: 'maquillaje-verde-3.jpg' },
-    { src: '../lizbyclaudia/assets/images/green-makeup-4.jpg', alt: 'maquillaje-verde-4.jpg' },
-    { src: '../lizbyclaudia/assets/images/green-makeup-5.jpg', alt: 'maquillaje-verde-5.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/green-makeup-1.jpg', alt: 'maquillaje-verde-1.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/green-makeup-2.jpg', alt: 'maquillaje-verde-2.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/green-makeup-3.jpg', alt: 'maquillaje-verde-3.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/green-makeup-4.jpg', alt: 'maquillaje-verde-4.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/green-makeup-5.jpg', alt: 'maquillaje-verde-5.jpg' },
 ]
 
 export const RED_MAKEUP = [
-    { src: '../lizbyclaudia/assets/images/red-makeup-open-eyes.jpg', alt: 'maquillaje-rojo-ojos-abiertos.jpg' },
-    { src: '../lizbyclaudia/assets/images/red-makeup-close-eyes.jpg', alt: 'maquillaje-verde-ojos-cerrados.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/red-makeup-open-eyes.jpg', alt: 'maquillaje-rojo-ojos-abiertos.jpg' },
+    { src: '../lizbyclaudia/src/assets/images/red-makeup-close-eyes.jpg', alt: 'maquillaje-verde-ojos-cerrados.jpg' },
 ]
