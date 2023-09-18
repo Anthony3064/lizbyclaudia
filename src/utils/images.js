53e0a360f32e64f4f86bc3989ff3086207e66d15
Anthony3064 <anthonyestiven3064@gmail.com>
2023-09-17 20:54:16 -0600

    Se agrega el new URL a todos los paths de imágenes

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,22 +1,22 @@
 export const FIRE_OUTLINE_MAKEUP = [
-    { src: './lizbyclaudia/src/assets/images/fire-outline-makeup.jpg', alt: 'delineado-fuego.jpg' },
-    { src: './lizbyclaudia/src/assets/images/fire-outline-makeup-close-eyes.jpg', alt: 'delineado-fuego-ojos-abiertos.jpg' },
+    { src: new URL('../assets/images/fire-outline-makeup.jpg', import.meta.url).href, alt: 'delineado-fuego.jpg' },
+    { src: new URL('../assets/images/fire-outline-makeup-close-eyes.jpg', import.meta.url).href, alt: 'delineado-fuego-ojos-abiertos.jpg' },
 ]
 
 export const BLUE_MAKEUP = [
-    { src: './lizbyclaudia/src/assets/images/blue-makeup.jpg', alt: 'maquillaje-azul.jpg' },
-    { src: './lizbyclaudia/src/assets/images/blue-makeup-complete.jpg', alt: 'maquillaje-azul-completo.jpg' },
+    { src: new URL('../assets/images/blue-makeup.jpg', import.meta.url), alt: 'maquillaje-azul.jpg' },
+    { src: new URL('../assets/images/blue-makeup-complete.jpg', import.meta.url), alt: 'maquillaje-azul-completo.jpg' },
 ]
 
 export const GREEN_MAKEUP = [
-    { src: '../lizbyclaudia/src/assets/images/green-makeup-1.jpg', alt: 'maquillaje-verde-1.jpg' },
-    { src: '../lizbyclaudia/src/assets/images/green-makeup-2.jpg', alt: 'maquillaje-verde-2.jpg' },
-    { src: '../lizbyclaudia/src/assets/images/green-makeup-3.jpg', alt: 'maquillaje-verde-3.jpg' },
-    { src: '../lizbyclaudia/src/assets/images/green-makeup-4.jpg', alt: 'maquillaje-verde-4.jpg' },
-    { src: '../lizbyclaudia/src/assets/images/green-makeup-5.jpg', alt: 'maquillaje-verde-5.jpg' },
+    { src: new URL('../assets/images/green-makeup-1.jpg', import.meta.url), alt: 'maquillaje-verde-1.jpg' },
+    { src: new URL('../assets/images/green-makeup-2.jpg', import.meta.url), alt: 'maquillaje-verde-2.jpg' },
+    { src: new URL('../assets/images/green-makeup-3.jpg', import.meta.url), alt: 'maquillaje-verde-3.jpg' },
+    { src: new URL('../assets/images/green-makeup-4.jpg', import.meta.url), alt: 'maquillaje-verde-4.jpg' },
+    { src: new URL('../assets/images/green-makeup-5.jpg', import.meta.url), alt: 'maquillaje-verde-5.jpg' },
 ]
 
 export const RED_MAKEUP = [
-    { src: '../lizbyclaudia/src/assets/images/red-makeup-open-eyes.jpg', alt: 'maquillaje-rojo-ojos-abiertos.jpg' },
-    { src: '../lizbyclaudia/src/assets/images/red-makeup-close-eyes.jpg', alt: 'maquillaje-verde-ojos-cerrados.jpg' },
+    { src: new URL('../assets/images/red-makeup-open-eyes.jpg', import.meta.url), alt: 'maquillaje-rojo-ojos-abiertos.jpg' },
+    { src: new URL('../assets/images/red-makeup-close-eyes.jpg', import.meta.url), alt: 'maquillaje-verde-ojos-cerrados.jpg' },
 ]
